579010aa5cc0ac8e6b7e95cc5a70c59e4c8dbccf
fyra <fyra@foxmail.com>
2024-11-27 01:45:20 +0800

    esp32c3 ver

--- main/servo.c
+++ main/servo.c
@@ -1,6 +1,6 @@
 #include "servo.h"
 
-#include "driver/mcpwm_prelude.h"
+#include "driver/ledc.h"
 #include "esp_log.h"
 
 #include "freertos/FreeRTOS.h"
@@ -11,6 +11,12 @@
 
 static const char *TAG = "SERVO";
 static QueueHandle_t servo_queue[3];
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_DUTY_RES           LEDC_TIMER_14_BIT // Set duty resolution to 14 bits
+#define LEDC_FREQUENCY          (50) // Frequency in Hertz. Set frequency at 50 Hz
+
 
 #pragma pack(push, 1)
 typedef struct {
@@ -30,7 +36,6 @@ void put_servo_data(int servo_num, float start, float end, float t_seconds)
     xQueueSend(servo_queue[servo_num], &data, portMAX_DELAY);
 }
 
-
 static void servo_loop_task(void *pvParameters)
 {
     int servo_num = *((int*)pvParameters);
@@ -45,117 +50,55 @@ static void servo_loop_task(void *pvParameters)
 }
 
 /*########################### PWM #################################*/
-mcpwm_cmpr_handle_t comparators[3];
-
-static void setup_sync_strategy(mcpwm_timer_handle_t timers[])
+// 对于ESP32C3，使用LEDC驱动PWM
+static void led_pwm_init()
 {
-    //       soft
-    //        |
-    //        v
-    //    +-timer0--+
-    //    |         |
-    //    v         v
-    // timer1    timer2
-    ESP_LOGI(TAG, "Create software sync source");
-    mcpwm_sync_handle_t soft_sync_source = NULL;
-    mcpwm_soft_sync_config_t soft_sync_config = {};
-    ESP_ERROR_CHECK(mcpwm_new_soft_sync_src(&soft_sync_config, &soft_sync_source));
-
-    ESP_LOGI(TAG, "Create timer sync source to propagate the sync event");
-    mcpwm_sync_handle_t timer_sync_source;
-    mcpwm_timer_sync_src_config_t timer_sync_config = {
-        .flags.propagate_input_sync = true,
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
     };
-    ESP_ERROR_CHECK(mcpwm_new_timer_sync_src(timers[0], &timer_sync_config, &timer_sync_source));
-
-    ESP_LOGI(TAG, "Set sync phase for timers");
-    mcpwm_timer_sync_phase_config_t sync_phase_config = {
-        .count_value = 0,
-        .direction = MCPWM_TIMER_DIRECTION_UP,
-        .sync_src = soft_sync_source,
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+    ledc_channel_config_t ledc_channel[3] = {
+        {
+            .speed_mode     = LEDC_MODE,
+            .channel        = LEDC_CHANNEL_0,
+            .timer_sel      = LEDC_TIMER,
+            .intr_type      = LEDC_INTR_DISABLE,
+            .gpio_num       = SERVO_PULSE_GPIO_0,
+            .duty           = 0, // Set duty to 0%
+            .hpoint         = 0
+        },
+        {
+            .speed_mode     = LEDC_MODE,
+            .channel        = LEDC_CHANNEL_1,
+            .timer_sel      = LEDC_TIMER,
+            .intr_type      = LEDC_INTR_DISABLE,
+            .gpio_num       = SERVO_PULSE_GPIO_1,
+            .duty           = 0, // Set duty to 0%
+            .hpoint         = 0
+        },
+        {
+            .speed_mode     = LEDC_MODE,
+            .channel        = LEDC_CHANNEL_2,
+            .timer_sel      = LEDC_TIMER,
+            .intr_type      = LEDC_INTR_DISABLE,
+            .gpio_num       = SERVO_PULSE_GPIO_2,
+            .duty           = 0, // Set duty to 0%
+            .hpoint         = 0
+        }
     };
-    ESP_ERROR_CHECK(mcpwm_timer_set_phase_on_sync(timers[0], &sync_phase_config));
-    sync_phase_config.sync_src = timer_sync_source;
-    for (int i = 1; i < 3; ++i) {
-        ESP_ERROR_CHECK(mcpwm_timer_set_phase_on_sync(timers[i], &sync_phase_config));
+    for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel[i]));
     }
-
-    ESP_LOGI(TAG, "Trigger the software sync event");
-    ESP_ERROR_CHECK(mcpwm_soft_sync_activate(soft_sync_source));
 }
 
-void mcpwm_init_grou()
+static uint32_t duty_f2i(float duty, uint32_t duty_resolution)
 {
-    ESP_LOGI(TAG, "Create timers");
-    mcpwm_timer_handle_t timers[3];
-    mcpwm_timer_config_t timer_config = {
-        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
-        .group_id = 0,
-        .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
-        .period_ticks = SERVO_TIMEBASE_PERIOD,
-        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
-    };
-    for (int i = 0; i < 3; i++) {
-        ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timers[i]));
-    }
-
-    ESP_LOGI(TAG, "Create operators");
-    mcpwm_oper_handle_t operators[3];
-    mcpwm_operator_config_t operator_config = {
-        .group_id = 0, // operator should be in the same group of the above timers
-    };
-    for (int i = 0; i < 3; ++i) {
-        ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &operators[i]));
-    }
-
-    ESP_LOGI(TAG, "Connect timers and operators with each other");
-    for (int i = 0; i < 3; i++) {
-        ESP_ERROR_CHECK(mcpwm_operator_connect_timer(operators[i], timers[i]));
-    }
-
-    ESP_LOGI(TAG, "Create comparators");
-    mcpwm_comparator_config_t compare_config = {
-        .flags.update_cmp_on_tez = true,
-    };
-    for (int i = 0; i < 3; i++) {
-        ESP_ERROR_CHECK(mcpwm_new_comparator(operators[i], &compare_config, &comparators[i]));
-        // init compare for each comparator
-        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], 200));
-    }
-
-    ESP_LOGI(TAG, "Create generators");
-    mcpwm_gen_handle_t generators[3];
-    const int gen_gpios[3] = {SERVO_PULSE_GPIO_0, SERVO_PULSE_GPIO_1, SERVO_PULSE_GPIO_2};
-    mcpwm_generator_config_t gen_config = {};
-    for (int i = 0; i < 3; i++) {
-        gen_config.gen_gpio_num = gen_gpios[i];
-        ESP_ERROR_CHECK(mcpwm_new_generator(operators[i], &gen_config, &generators[i]));
-    }
-
-    ESP_LOGI(TAG, "Set generator actions on timer and compare event");
-    for (int i = 0; i < 3; i++) {
-        ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generators[i],
-                                                                  // when the timer value is zero, and is counting up, set output to high
-                                                                  MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
-        ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generators[i],
-                                                                    // when compare event happens, and timer is counting up, set output to low
-                                                                    MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparators[i], MCPWM_GEN_ACTION_LOW)));
-    }
-
-    ESP_LOGI(TAG, "Start timers one by one, so they are not synced");
-    for (int i = 0; i < 3; i++) {
-        ESP_ERROR_CHECK(mcpwm_timer_enable(timers[i]));
-        ESP_ERROR_CHECK(mcpwm_timer_start_stop(timers[i], MCPWM_TIMER_START_NO_STOP));
-    }
-
-    ESP_LOGI(TAG, "Setup sync strategy");
-    setup_sync_strategy(timers);
-
-    ESP_LOGI(TAG, "Now the output PWMs should in sync");
-    for (int i = 0; i < 3; ++i) {
-        // remove the force level on the generator, so that we can see the PWM again
-        ESP_ERROR_CHECK(mcpwm_generator_set_force_level(generators[i], -1, true));
-    }
+    return (uint32_t)(duty * (float)(1 << duty_resolution));
 }
 
 void set_servo_pulse_width(int servo_num, int pulse_width)
@@ -164,13 +107,15 @@ void set_servo_pulse_width(int servo_num, int pulse_width)
         ESP_LOGE(TAG, "Invalid servo number %d", servo_num);
         return;
     }
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[servo_num], pulse_width));
+    
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, (ledc_channel_t)servo_num, pulse_width));
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, (ledc_channel_t)servo_num));
 }
 
 void servo_tasks_init()
 {
     ESP_LOGI(TAG, "Setup servo");
-    mcpwm_init_grou();
+    led_pwm_init();
     for (int i = 0; i < 3; i++) {
         servo_queue[i] = xQueueCreate(1, sizeof(servo_data_t));
         xTaskCreate(servo_loop_task, "servo_tasks", 4096, &i, 10, NULL);
@@ -183,7 +128,7 @@ void fivetimesInterpolation(int myservo, float thetai, float thetaf, float t_sec
     // 定义插值时间总长度 (ms)，可以根据需求调整
     // float T = 2000.0; // 插值时间为2秒 (2000 ms)
     float T = t_seconds * 1000.0; // 插值时间为2秒 (2000 ms)
-    int steps = 50;   // 插值步数，越多越平滑
+    int steps = 20;   // 插值步数，越多越平滑
     float dt = T / steps;
 
     // 五次插值多项式系数
@@ -200,8 +145,8 @@ void fivetimesInterpolation(int myservo, float thetai, float thetaf, float t_sec
         float theta = a0 + a1 * t + a2 * t * t + a3 * t * t * t + a4 * t * t * t * t + a5 * t * t * t * t * t;
         
         // 将插值角度设置到舵机
-        int pulse_width = (int)(theta * 2000.0 / 180.0 + 500); // 将角度转换为脉宽值
-        set_servo_pulse_width(myservo, pulse_width);
+        float pulse_width = (theta * 2.0 / 180.0 + 0.5) / 20.0; // 将角度转换为占空比;20.0代表20ms
+        set_servo_pulse_width(myservo, duty_f2i(pulse_width, LEDC_DUTY_RES));
         vTaskDelay(pdMS_TO_TICKS(dt));
     }
 }
--- main/servo.h
+++ main/servo.h
@@ -1,11 +1,9 @@
 #ifndef SERVO_H
 #define SERVO_H
 
-#define SERVO_PULSE_GPIO_0             1        // GPIO connects to the PWM signal line
+#define SERVO_PULSE_GPIO_0             5        // GPIO connects to the PWM signal line
 #define SERVO_PULSE_GPIO_1             2        // GPIO connects to the PWM signal line
 #define SERVO_PULSE_GPIO_2             3        // GPIO connects to the PWM signal line
-#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
-#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms
 
 void servo_tasks_init();
 void fivetimesInterpolation(int myservo, float thetai, float thetaf, float t);
